bdd21da50bc8fb97b602f2999ff409710d61773e
Dr. Setz <thomas@setz.de>
2019-09-25 23:05:12 +0200

    fixed tests - added debug toolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask  # new
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_debugtoolbar import DebugToolbarExtension  
 
 # instantiate the db
 db = SQLAlchemy()
-
+toolbar = DebugToolbarExtension()
 
 # new
 def create_app(script_info=None):
@@ -20,6 +20,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,15 +5,17 @@ class BaseConfig:
     """Base configuration"""
 
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
     SECRET_KEY = "my_precious"
+    DEBUG_TB_ENABLED = False              
+    DEBUG_TB_INTERCEPT_REDIRECTS = False  
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # new
-
+    DEBUG_TB_ENABLED = True 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
