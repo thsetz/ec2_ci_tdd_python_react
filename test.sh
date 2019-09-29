8cb3eb94624c7d67de46fa214f48b4dfacca77ae
Dr. Setz <thomas@setz.de>
2019-09-29 19:20:44 +0200

    adapted ci files

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-type=$1
+env=$1
 fails=""
 
 inspect() {
@@ -10,35 +10,8 @@ inspect() {
   fi
 }
 
-# run server-side tests
-server() {
-  docker-compose up -d --build
-  docker-compose exec users python manage.py test
-  inspect $? users
-  docker-compose exec users flake8 project
-  inspect $? users-lint
-  docker-compose down
-}
-
-# run client-side tests
-client() {
-  docker-compose up -d --build
-  export CI=True && docker-compose exec client npm test -- --coverage
-  inspect $? client
-  docker-compose down
-}
-
-# run e2e tests
-e2e() {
-  docker-compose -f docker-compose-prod.yml up -d --build
-  docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
-  inspect $? e2e
-  docker-compose -f docker-compose-prod.yml down
-}
-
-# run all tests
-all() {
+# run client and server-side tests
+dev() {
   docker-compose up -d --build
   docker-compose exec users python manage.py test
   inspect $? users
@@ -47,38 +20,37 @@ all() {
   export CI=true && docker-compose exec client npm test -- --coverage
   inspect $? client
   docker-compose down
-  e2e
+}
+
+# run e2e tests
+e2e() {
+  docker-compose -f docker-compose-$1.yml up -d --build
+  docker-compose -f docker-compose-$1.yml run users python manage.py recreate_db
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  inspect $? e2e
+  docker-compose -f docker-compose-$1.yml down
 }
 
 # run appropriate tests
-if [[ "${type}" == "server" ]]; then
-  echo "\n"
-  echo "Running server-side tests!\n"
-  server
-elif [[ "${type}" == "client" ]]; then
-  echo "\n"
-  echo "Running client-side tests!\n"
-  client
-elif [[ "${type}" == "e2e" ]]; then
-  echo "\n"
-  echo "Running e2e tests!\n"
-  e2e
+if [[ "${env}" == "development" ]]; then
+  echo "Running client and server-side tests!"
+  dev
+elif [[ "${env}" == "staging" ]]; then
+  echo "Running e2e tests!"
+  e2e stage
+elif [[ "${env}" == "production" ]]; then
+  echo "Running e2e tests!"
+  e2e prod
 else
-  echo "\n"
-  echo "Running all tests!\n"
-  all
+  echo "Running client and server-side tests!"
+  dev
 fi
 
 # return proper code
 if [ -n "${fails}" ]; then
-  echo "\n"
   echo "Tests failed: ${fails}"
   exit 1
 else
-  echo "\n"
   echo "Tests passed!"
   exit 0
 fi
-
-
-
