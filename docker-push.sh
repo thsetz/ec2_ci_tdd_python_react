e0d976650f810a360d80f433960bf5dc03905a91
Dr. Setz <thomas@setz.de>
2019-09-29 19:38:17 +0200

    fix repo location

--- docker-push.sh
+++ docker-push.sh
@@ -18,7 +18,7 @@ then
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
     eval $(aws ecr get-login --region eu-central-1  --no-include-email)
     export TAG=$TRAVIS_BRANCH
-    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
