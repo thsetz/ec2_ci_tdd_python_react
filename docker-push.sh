f1ff2de2a32d90ef89f7dd35e54f59fedb1ba9c6
Dr. Setz <thomas@setz.de>
2019-10-01 15:02:11 +0200

    add aws staging dns adress

--- docker-push.sh
+++ docker-push.sh
@@ -34,7 +34,7 @@ then
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-${DOCKER_ENV} --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-${DOCKER_ENV} --build-arg REACT_APP_USERS_SERVICE_URL=${REACT_APP_USERS_SERVICE_URL}
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
