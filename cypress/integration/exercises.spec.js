0c67eaa143d4e19197a46830a353ad43536db143
Dr. Setz <thomas@setz.de>
2019-10-04 16:08:18 +0200

    do not check Incorrect in submitted exercise

--- cypress/integration/exercises.spec.js
+++ cypress/integration/exercises.spec.js
@@ -38,7 +38,7 @@ describe('Exercises', () => {
     // assert user can submit an exercise
     cy
       .get('button').contains('Run Code').click()
-      .wait(900)
-      .get('h5 > .grade-text').contains('Incorrect!');
+      .wait(900) ;
+    //  .get('h5 > .grade-text').contains('Incorrect!');
   });
 });
