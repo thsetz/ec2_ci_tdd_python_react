b0a9354cb7ea46b11d5f17811a4d7e7cf49414b3
Dr. Setz <thomas@setz.de>
2019-10-09 21:08:07 +0200

    add to styling

--- services/client/src/components/NavBar.jsx
+++ services/client/src/components/NavBar.jsx
@@ -6,43 +6,53 @@ const NavBar = (props) => (
    // <nav className="navbar is-dark" role="navigation" aria-label="main navigation" background="tan_paper.gif"> 
   <nav  role="navigation" aria-label="main navigation" background="tan_paper.gif">
     <section className="container">
-      <div className="navbar-brand">
-            <strong className="navbar-item">{props.title}</strong>
-            <span
-              className="nav-toggle navbar-burger"
-              onClick={() => {
-                let toggle = document.querySelector(".nav-toggle");
-                let menu = document.querySelector(".navbar-menu");
-                toggle.classList.toggle("is-active"); menu.classList.toggle("is-active");
-              }}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          </div>
-          <div className="navbar-menu">
-            <div className="navbar-start">
-              <Link to="/" className="navbar-item">Home</Link>
-              <Link to="/about" className="navbar-item">About</Link>
-              <Link to="/all-users" className="navbar-item">Users</Link>
-              {props.isAuthenticated &&
-                <Link to="/status" className="navbar-item">User Status</Link>
-              }
-              <a href="/swagger" className="navbar-item">Swagger</a>
-            </div>
-            <div className="navbar-end">
-              {!props.isAuthenticated &&
-                <div className="navbar-item">
-                  <Link to="/register" className="button is-primary">Register</Link>
-                  &nbsp;
-                  <Link to="/login" className="button is-link">Log In</Link>
+        <table>
+          <tr><td> <img src ="logo_transl.gif" style={{"border-style":"none", "float":"left", "width":"6%" , alt:"EDV Beratung Dr.-Ing. Setz"}}/>  </td> 
+             <td width="60%">
+              <div className="navbar-brand">
+                <h1 style={{ "height":"80px", "font-family":"verdana,helvetica", "font-size":"28px","color":"navy", textAlign:"left", fontWeight: 'bold' }}  className="navbar-item">{props.title}</h1>
+                <span
+                  className="nav-toggle navbar-burger"
+                  onClick={() => {
+                    let toggle = document.querySelector(".nav-toggle");
+                    let menu = document.querySelector(".navbar-menu");
+                    toggle.classList.toggle("is-active"); menu.classList.toggle("is-active");
+                  }}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+             </div>
+               </td>
+               <td> 
+                   <img src ="logo_transr.gif" style={{"border-style":"none", "float":"right", "width":"60%" , alt:"EDV Beratung Dr.-Ing. Setz"}}/> 
+               </td> </tr>
+       <tr><td>
+              <div className="navbar-menu">
+                <div className="navbar-start">
+                  <Link to="/" className="navbar-item">Home</Link>
+                  <Link to="/about" className="navbar-item">About</Link>
+                  <Link to="/all-users" className="navbar-item">Users</Link>
+                  {props.isAuthenticated &&
+                    <Link to="/status" className="navbar-item">User Status</Link>
+                  }
+                  <a href="/swagger" className="navbar-item">Swagger</a>
                 </div>
-              }
-              {props.isAuthenticated &&
-                <Link to="/logout" className="navbar-item">Log Out</Link>
-              }
-            </div>
-      </div>
+                <div className="navbar-end">
+                  {!props.isAuthenticated &&
+                    <div className="navbar-item">
+                      <Link to="/register" className="button is-primary">Register</Link>
+                      &nbsp;
+                      <Link to="/login" className="button is-link">Log In</Link>
+                    </div>
+                  }
+                  {props.isAuthenticated &&
+                    <Link to="/logout" className="navbar-item">Log Out</Link>
+                  }
+                </div>
+              </div>
+      </td></tr>
+      </table>
     </section>
   </nav>
 )
