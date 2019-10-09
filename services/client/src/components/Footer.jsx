052f4367647928024a970dc4e891ef9159feeb9c
Dr. Setz <thomas@setz.de>
2019-10-09 17:52:15 +0200

    changed style

--- services/client/src/components/Footer.jsx
+++ services/client/src/components/Footer.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import './Footer.css';
+//import './Footer.css';
 
 const Footer = (props) => (
-  <footer className="footer">
-    <div className="container">
-      <small className="has-text-grey">
-        <span>Copyright 2019 <a href="http://testdriven.io">TestDriven.io</a>.</span>
-      </small>
+  <footer  background="tan_paper.gif">
+    <div className="container" >
+    <table>
+       <thead>
+       <tr><td> <img src ="logo_transl.gif" style={{float: 'left', width: '3%'}} alt="footerlogo_left" /> </td> 
+        <td>
+          <center>
+            <a href="/fix/impressum">Impressum</a>
+            <a href="/fix/datenschutz">Datenschutz</a><br/>
+            <span>Copyright 1998 - 2019 <a href="http://setz.de">Edv Beratung Dr. Setz</a>.</span>
+          </center>
+       </td>
+       <td> <img src ="logo_transr.gif" style={{float:'right', width: '5%'}} alt="footerlogo_right" /> </td></tr>
+       </thead>
+    </table>
     </div>
   </footer>
 )
